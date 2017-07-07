2b20cfac18bd86aa92ffb7aeec08b9a61e806be8
Yann Carlen <yann.carlen@epitech.eu>
2017-07-08 00:25:38 +0200

    add route type and fix chunk

--- rest/index.js
+++ rest/index.js
@@ -12,19 +12,22 @@ const home = (req, res) => {
 }
 
 const router = {
-    '/': home,
-    '': home
+    'GET /': home,
+    'GET ': home
 }
+
+const getRoute = (method, urlP) => `${method} ${urlP.pathname}`
 
 const request = (req, res) => {
     let body = ''
     const urlP = url.parse(req.url)
+    const method = req.method
     
     //TODO(carlendev) set header
-    req.on('data', chunk => append(chunk, urlP))
+    req.on('data', chunk => append(chunk, body))
     //TODO(carlendev) add 404
     //TODO(carlendev) check GET/POST
-    req.on('end', () => router[urlP.pathname](req, res))
+    req.on('end', () => router[getRoute(method, urlP)](req, res))
 }
 
 http.createServer(request).listen(port, () => wesh(`Server listen on ${port}`))
